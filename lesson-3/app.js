1668ad0e6bd48ce18328c7354720d8a3088c0dd1
Muhammad <mitcao.000@gmail.com>
2025-04-13 16:45:13 +0300

    Lesson 3 string methods

--- lesson-3/app.js
+++ lesson-3/app.js
@@ -20,3 +20,24 @@ console.log(result)
 
 let index = fullName.indexOf()
 console.log(index)
+
+let result2 = email.lastIndexOf(o)
+console.log(result2)
+
+let result3 = email.slice(0, 1)
+console.log(result3)
+
+let result4 = email.substring(1, 10)
+console.log(result4)
+
+let result5 = email.replace('a', 'b')
+console.log(result5)
+
+let result6 = email.charAt()
+console.log(result6)
+
+let result7 = email.trim()
+console.log(result7.length, email.length)
+
+let result8 = email.split()
+console.log(result8)
