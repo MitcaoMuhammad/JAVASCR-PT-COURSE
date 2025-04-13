8397da25754098fae0ddd7da02234e33d1fd6a47
Muhammad <mitcao.000@gmail.com>
2025-04-13 16:01:37 +0300

    Lesson 2 Variables

--- lesson-2/app.js
+++ lesson-2/app.js
@@ -1,2 +1,17 @@
 console.log('1')
 console.log('2')
+
+let ism = 'Ahror'
+let yosh = '24'
+console.log(ism, yosh)
+
+yosh = '25'
+
+console.log(yosh)
+
+const familia = 'Said'
+console.log(familia)
+
+var joy = 'Istanbul'
+joy = 'Islambul'
+console.log(joy)
