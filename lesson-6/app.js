e225a9bc80b92b1862c1408fba0f480223fa29ca
Muhammad <mitcao.000@gmail.com>
2025-04-13 17:59:40 +0300

    Lesson 6 Booleans & Comparisons

---
+++ lesson-6/app.js
@@ -0,0 +1,23 @@
+console.log(true, false, 'true', 'false')
+
+let email = 'akhrorweb@gmail.com'
+
+let result = email.includes('!')
+
+console.log(result)
+
+let age = 25
+
+console.log(age == 25)
+console.log(age == 30)
+console.log(age != 30)
+console.log(age > 20)
+console.log(age < 20)
+console.log(age <= 25)
+console.log(age >= 25)
+
+let ism = 'dhror'
+
+console.log(ism == 'ahror')
+console.log(ism == 'Ahror')
+console.log('dhror' >= 'dhror')
