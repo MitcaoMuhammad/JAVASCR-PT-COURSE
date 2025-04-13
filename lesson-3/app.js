02bb9a4de2c332569235b87a2414d712a3ee55fe
Muhammad <mitcao.000@gmail.com>
2025-04-13 16:31:20 +0300

    Lesson 3 string

---
+++ lesson-3/app.js
@@ -0,0 +1,22 @@
+console.log('Hello world +!')
+
+let email = 'mitcao.0000@gmail.com'
+console.log(email)
+
+let firstName = 'Mit'
+let lastName = 'Cao'
+
+let fullName = firstName + ' ' + lastName
+console.log(fullName)
+
+console.log(fullName[2])
+
+console.log(fullName.length)
+
+console.log(fullName.toLowerCase())
+
+let result = fullName.toLowerCase()
+console.log(result)
+
+let index = fullName.indexOf()
+console.log(index)
