b0aeae64a8e071361d2cc505d835382242999885
Muhammad <mitcao.000@gmail.com>
2025-04-13 17:56:00 +0300

    Lesson 5 Array

---
+++ lesson-5/app.js
@@ -0,0 +1,24 @@
+const mehmon1 = 'Azizbek'
+const mehmon2 = 'MuhammadSaid'
+const mehmon3 = 'Asror'
+const mehmon4 = 'Diyorbek'
+
+const mehmonlar = ['Azizbek', 'MuhmmadSaid', 'Asror', 'Diyorbek']
+
+mehmonlar[0] = 'Ahror'
+console.log(mehmonlar)
+
+let numbers = [1, 2, 3, 4, 5, 6, 123]
+
+property
+const result1 = numbers.length
+
+console.log(result1)
+
+let result2 = mehmonlar.indexOf('Diyorbek')
+let result3 = mehmonlar.concat(numbers)
+
+mehmonlar.push('Davlatbek')
+mehmonlar.pop('Diyorbek')
+
+console.log(mehmonlar)
