af7b9df445f02d65d69e64ca7c92cbf20512e160
Muhammad <mitcao.000@gmail.com>
2025-04-14 14:28:55 +0300

    lesson 22 Arrow function

---
+++ lesson-22/app.js
@@ -0,0 +1,10 @@
+// const calcArea = function (radius) {
+// 	return 3.14 * radius ** 2
+// }
+
+const calcArea = () => {
+	return 3.14 * radius ** 2
+}
+
+const area = calcArea(5)
+console.log(area)
