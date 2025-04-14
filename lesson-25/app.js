e2576b4ce0f62f85076c2f81d4e51df6bb9f8c9b
Muhammad <mitcao.000@gmail.com>
2025-04-14 21:31:33 +0300

    lesson 24 Objects

---
+++ lesson-25/app.js
@@ -0,0 +1,19 @@
+const user = {
+	name: 'Ahror',
+	age: 24,
+	email: 'akhrorteacher@gmail.com',
+	location: "Farg'ona",
+	lang: ['uzbek', 'russian', 'english'],
+}
+
+console.log(user)
+console.log(user.email)
+
+user.age = 25
+console.log(user.age)
+
+console.log(user['location'])
+user['name'] = 'Doniyor'
+console.log(user['name'])
+
+console.log(typeof user)
