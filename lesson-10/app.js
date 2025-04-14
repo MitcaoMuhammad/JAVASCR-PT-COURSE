430ae014ae37098b8e3b1f1a850ab037152657fb
Muhammad <mitcao.000@gmail.com>
2025-04-14 11:56:48 +0300

    lesson 10 while loop

---
+++ lesson-10/app.js
@@ -0,0 +1,15 @@
+const names = ['ahror', 'sardor', 'doniyor']
+
+let i = 0
+
+while (i < names.length) {
+	console.log(names[i].toUpperCase())
+	i++
+}
+
+let i1 = 5
+
+do {
+	console.log(i1)
+	i1++
+} while (i1 < 5)
--- lesson-9/app.js
+++
@@ -1,9 +0,0 @@
-for (let i = 0; i < 10; i++) {
-	console.log(i)
-}
-
-const names = ['ahror', 'sardor', 'doniyor']
-
-for (let i = 0; i < names.length; i++) {
-	console.log(names[i])
-}
