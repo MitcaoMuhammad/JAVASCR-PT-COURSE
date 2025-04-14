116ff0e083c08c674e004497ce06dab1e77cd5dc
Muhammad <mitcao.000@gmail.com>
2025-04-14 12:58:05 +0300

    lesson 15 Switch Case

---
+++ lesson-15/app.js
@@ -0,0 +1,18 @@
+const baho = 'D'
+
+switch (baho) {
+	case 'A':
+		console.log('Sizning bahoyingiz A')
+		break
+	case 'B':
+		console.log('Sizning bahoyingiz B')
+		break
+	case 'C':
+		console.log('Sizning bahoyingiz C')
+		break
+	case 'D':
+		console.log('Sizning bahoyingiz D')
+		break
+	default:
+		console.log('Sizning bahoyingiz e')
+}
