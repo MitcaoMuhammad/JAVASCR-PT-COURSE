2747356b703af9cd154d3d2943a5d5ee7549160c
Muhammad <mitcao.000@gmail.com>
2025-04-14 12:10:00 +0300

    lesson 11 If Statement

---
+++ lesson-11/app.js
@@ -0,0 +1,17 @@
+const age = 25
+
+if (age > 20) {
+	console.log('siz 20 yoshda kattasiz.')
+}
+
+const mehmonlar = ['azizbek', 'lazizbek', 'kebabek', 'pizabek']
+
+if (mehmonlar.length > 3) {
+	console.log('yrtarlicha mehmonlar mavjud')
+}
+
+const parol = 'pass'
+
+if (parol.length >= 8) {
+	console.log('Sizning parolingiz yitarlicha kuchli')
+}
