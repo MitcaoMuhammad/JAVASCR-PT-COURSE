d5a8c0d29c5ad1b43d86b5aeb80ad30bbbcacb2b
Muhammad <mitcao.000@gmail.com>
2025-04-14 11:50:07 +0300

    lesson 9 for loop

--- lesson-8/app.js
+++
@@ -1,7 +0,0 @@
-let ism = prompt('Ismingizni kiriting:')
-
-let birinchiHarf = ism.charAt().toUpperCase()
-let davomi = ism.slice(1).toLowerCase()
-let result = birinchiHarf + davomi
-
-alert(`Salom, ${result}`)
