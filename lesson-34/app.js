06e70d21502374784b0742c6f48e9e7bd54ccf67
Muhammad <mitcao.000@gmail.com>
2025-04-15 21:27:52 +0300

    lesson 35 add & remove class

--- lesson-34/app.js
+++
@@ -1,9 +0,0 @@
-const heading = document.querySelector('h1')
-
-// heading.setAttribute('style', 'margin: 50px')
-console.log(heading.style)
-heading.style.margin = '50px'
-heading.style.color = 'crimson'
-heading.style.fontSize = '50px'
-heading.style.margin = ''
-heading.style.backgroundColor = '#ccc'
