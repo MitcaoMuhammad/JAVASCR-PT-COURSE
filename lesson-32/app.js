36942a6c75c57e1ef40916a00ae4aac0311c3ed0
Muhammad <mitcao.000@gmail.com>
2025-04-15 20:46:16 +0300

    lesson 31 textContent/innerHTML

---
+++ lesson-32/app.js
@@ -0,0 +1,16 @@
+const title = document.getElementById('title')
+title.textContent += ' New Text'
+
+const listItem = document.querySelectorAll('.list-item')
+
+listItem.forEach(item => {
+	item.textContent += ' darslari'
+})
+
+const names = ['Ahror', 'Sardor', 'Doniyor']
+
+const ol = document.querySelector('ol')
+
+names.forEach(name => {
+	ol.innerHTML += `<li>${name}</li>`
+})
