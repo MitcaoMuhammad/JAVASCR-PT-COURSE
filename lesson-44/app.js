657e030a4e7cc104cb143796d7fbb7e8261563f8
Muhammad <mitcao.000@gmail.com>
2025-04-16 13:49:28 +0300

    lesson 45 Event KeyCodes

--- lesson-44/app.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('keyup', e => {
-	if (e.key == 'm' && e.ctrlKey) console.log('GOOOL !!!')
-	// console.log('Code', e.code)
-	// console.log('key', e.key)
-	// console.log('keyCode', e.keyCode)
-})
