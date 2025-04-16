227f9634805ffca9c3f242880e5414cd6b4ee9c0
Muhammad <mitcao.000@gmail.com>
2025-04-16 19:56:15 +0300

    lesson 49 Ternary operation

---
+++ lesson-49/app.js
@@ -0,0 +1,16 @@
+const age = prompt('Yoshingizni kiriting', 18)
+
+/* if (age < 18) {
+	alert('siz hali voyaga yitmagansiz')
+} else {
+	alert('siz voyag yitgansiz !')
+} */
+
+/* age < 18 ? alert('siz hali voyaga yitmagansiz') : alert('siz voyag yitgansiz !') */
+
+/* age < 18
+	? alert(`siz armiya yoshidan ${18 - age} kichiksiz`)
+	: age < 28
+	? alert('siz armiyaga borishiyiz mumkin')
+	: alert(`siz armiya yoshidan ${age - 18} kattasiz`)
+*/
