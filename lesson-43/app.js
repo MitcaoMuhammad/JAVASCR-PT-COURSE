b53a1cbf1ba696671c5fed437351626d0369156a
Muhammad <mitcao.000@gmail.com>
2025-04-16 13:29:26 +0300

    lesson 42 regEX Live Input Validation

--- lesson-43/app.js
+++ lesson-43/app.js
@@ -1,10 +1,10 @@
 const signupForm = document.querySelector('#signup-form')
 // const username = document.querySelector('#username')
 const message = document.querySelector('.message')
+const regEX = /^[a-zA-Z0-9]{6,12}$/
 
 signupForm.addEventListener('submit', e => {
 	e.preventDefault()
-	const regEX = /^[a-zA-Z0-9]{6,12}$/
 	const inputVal = signupForm.username.value
 
 	if (regEX.test(inputVal)) {
@@ -12,5 +12,13 @@ signupForm.addEventListener('submit', e => {
 	} else {
 		message.textContent =
 			"Kiritilyotgan ma'lumot 6 va 12 orasida bo'lshi kerak. Shuningdek kichik va katta lotin harflaridan iborat bo'lshi zarur. Raqamlar ham ishitiok eshitishi mumkin."
+	}
+})
+
+signupForm.username.addEventListener('keyup', e => {
+	if (regEX.test(e.target.value)) {
+		signupForm.username.setAttribute('class', 'success')
+	} else {
+		signupForm.username.setAttribute('class', 'error')
 	}
 })
