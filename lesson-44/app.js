7ddb837ec0ee659c1025f86441e0d23e28575653
Muhammad <mitcao.000@gmail.com>
2025-04-16 13:41:16 +0300

    lesson 44 KeyboardEvent

---
+++ lesson-44/app.js
@@ -0,0 +1,6 @@
+document.addEventListener('keyup', e => {
+	if (e.key == 'm' && e.ctrlKey) console.log('GOOOL !!!')
+	// console.log('Code', e.code)
+	// console.log('key', e.key)
+	// console.log('keyCode', e.keyCode)
+})
