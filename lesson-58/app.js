2e539124cb09b408bba65fc4e402e1763529b17b
Muhammad <mitcao.000@gmail.com>
2025-04-19 18:28:57 +0300

    lesson 58 Request Status

--- lesson-58/app.js
+++ lesson-58/app.js
@@ -1,11 +1,12 @@
 const request = new XMLHttpRequest()
 
 request.addEventListener('readystatechange', () => {
-	if (request.readyState === 4) {
+	if (request.readyState === 4 && request.status === 200) {
 		console.log(request.responseText)
+	} else if (request.readyState === 4) {
+		console.log("Ma'lumotni olishning iloji bo'lmadi !!!")
 	}
 })
 
 request.open('GET', 'https://jsonplaceholder.typicode.com/todos/')
-
 request.send()
