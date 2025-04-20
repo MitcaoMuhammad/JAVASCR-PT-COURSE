140e7ac1fef4b198ec9d4b0e80de40c9e93ff623
Muhammad <mitcao.000@gmail.com>
2025-04-20 10:50:44 +0300

    lesson 63 Promise chaining

--- lesson-63/app.js
+++ lesson-63/app.js
@@ -19,6 +19,14 @@ const getTodos = resurse => {
 getTodos('todos/ahror.json')
 	.then(data => {
 		console.log(data)
+		return getTodos('todos/sardor.json')
+	})
+	.then(data => {
+		console.log(data)
+		return getTodos('todos/doniyor.json')
+	})
+	.then(data => {
+		console.log(data)
 	})
 	.catch(err => {
 		console.log(err)
