6748003a3c3e621c32a80982f93a3813d6a709d0
Muhammad <mitcao.000@gmail.com>
2025-04-20 13:37:22 +0300

    lesson 65 Fetch API

---
+++ lesson-65/app.js
@@ -0,0 +1,39 @@
+const APİ = 'https://restcountries.com/v3.1/all'
+
+const getData = resourcs => {
+	return new Promise((resolve, reject) => {
+		const request = new XMLHttpRequest()
+
+		request.addEventListener('readystatechange', () => {
+			if (request.readyState === 4 && request.status === 200) {
+				const data = JSON.parse(request.responseText)
+				resolve(data)
+			} else if (request.readyState === 4) {
+				reject('Error')
+			}
+		})
+
+		request.open('GET', resourcs)
+		request.send()
+	})
+}
+
+getData(APİ)
+	.then(data => {
+		console.log(data)
+	})
+	.catch(err => {
+		console.log(err)
+	})
+
+// fetch
+fetch(APİ)
+	.then(data => {
+		return data.json
+	})
+	.then(dataJson => {
+		console.log(dataJson)
+	})
+	.catch(err => {
+		console.log(err)
+	})
